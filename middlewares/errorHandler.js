17d88920fb03a5e9f6fcf34a7804fed4367edfab
Tenari <guilherme.tenarie@gmail.com>
2022-02-03 20:16:33 -0300

    Add error middleware and rule for no-repeated users at service post/user route

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, _next) => {
   if (code) {
     return res.status(code).json({ message });
   }
-  return res.status(500).json({ message: 'something went wrong ' });
+  return res.status(500).json({ message: 'something went wrong' });
 };
 
 module.exports = errorHandler;
